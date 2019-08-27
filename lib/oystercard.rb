203a6c433382de5224999a4d6edcf0595b4b88bd
Daniel Segall <segalld@gmail.com>
2019-08-27 15:43:22 +0100

    Add top up functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,28 @@
 class Oystercard
+  class BalanceLimitExceeded < RuntimeError
+  end
+
   attr_reader :balance
+
+  BALANCE_LIMIT = 90
 
   def initialize(balance = 0)
     @balance = balance
+  end
+
+  def top_up(amount)
+    raise_if_balance_exceeded(amount)
+
+    @balance += amount
+  end
+
+  private
+
+  def raise_if_balance_exceeded(amount)
+    raise BalanceLimitExceeded if over_limit?(amount)
+  end
+
+  def over_limit?(amount)
+    @balance + amount > BALANCE_LIMIT
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,19 @@
 require './lib/oystercard'
 
 describe Oystercard do
-  it "new card provides default balance of 0" do
+  it 'new card provides default balance of 0' do
     expect(subject.balance).to eq 0
+  end
+
+  describe '#top_up' do
+    it 'should add top up value to balance' do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+
+    it 'should raise error if balance would go over limit' do
+      expect { subject.top_up(100) }.to raise_error Oystercard::BalanceLimitExceeded
+    end
+
   end
 end
