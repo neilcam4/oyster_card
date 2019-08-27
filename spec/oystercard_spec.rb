599048bc60401a8efa1e6a00f16bcfe77ede80f1
Daniel Segall <segalld@gmail.com>
2019-08-27 15:50:37 +0100

    Use default balance limit in tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,8 +12,7 @@ describe Oystercard do
     end
 
     it 'should raise error if balance would go over limit' do
-      expect { subject.top_up(100) }.to raise_error Oystercard::BalanceLimitExceeded
+      expect { subject.top_up(Oystercard::BALANCE_LIMIT + 1) }.to raise_error Oystercard::BalanceLimitExceeded
     end
-
   end
 end
