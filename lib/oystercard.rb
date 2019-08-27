798f1e82768f29abeb67894a6aaf245562ccb8d9
Daniel Segall <segalld@gmail.com>
2019-08-27 16:19:41 +0100

    Add touch in and touch out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,6 +8,7 @@ class Oystercard
 
   def initialize(balance = 0)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -18,6 +19,18 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,12 +15,32 @@ describe Oystercard do
       expect { subject.top_up(Oystercard::BALANCE_LIMIT + 1) }.to raise_error Oystercard::BalanceLimitExceeded
     end
   end
-  
-  describe "#deduct" do
-    it "should deduct amount from balance" do
+
+  describe '#deduct' do
+    it 'should deduct amount from balance' do
       subject.top_up(10)
       subject.deduct(5)
       expect(subject.balance).to eq 5
+    end
+  end
+
+  describe 'journey progress' do
+    def in_journey?
+      subject.in_journey?
+    end
+
+    it 'should not be in journey by default' do
+      expect(in_journey?).to be false
+    end
+
+    it 'should be in journey after touching in' do
+      subject.touch_in
+      expect(in_journey?).to be true
+    end
+
+    it 'should not be in journey after touching out' do
+      subject.touch_out
+      expect(in_journey?).to be false
     end
   end
 end
